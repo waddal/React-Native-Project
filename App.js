3baec7a65f9d357fbf1f337c6141c82214ca5e14
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-03-06 15:49:17 -0800

    feat: app recognized as react-native. vscode debugger works properly. publish success.

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x = 'Jessie is WONDERFUL! 😍';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{x}</Text>
       <StatusBar style="auto" />
     </View>
   );
