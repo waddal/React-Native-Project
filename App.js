dc4bde5da2c9a5676e0e2180df9415d39638f637
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-03-07 15:10:37 -0800

    flex basics, flex grow and shrink

--- App.js
+++ App.js
@@ -1,37 +1,57 @@
-import {
-  Dimensions,
-  StyleSheet,
-  SafeAreaView,
-  View,
-  Platform,
-  StatusBar,
-} from "react-native";
-import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
-  // set 'orientation' to 'default' in app.json to enable portrait/landscape views
-  // console.log('Determine screen/window size', Dimensions.get("screen"));
-  // console.log(useDimensions());
-  // console.log(useDeviceOrientation());
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ 
-        backgroundColor: "dodgerblue", 
-        width: "100%", 
-        height: landscape ? "100%" : "30%"
-         }}></View>
-    </SafeAreaView>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center", // aligns line determines alignment of items in the line
+        alignContent: "center", // determines alignment of all content within container
+        flexWrap: "wrap",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          // flexBasis: 100, // width or height on primary axis
+          // flex: 1, // flexGrow has similar effect
+          flexShrink: 1, // if item overflows, shrinks to give room to other elements
+          height: 100,
+          width: 400
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          height: 100,
+          width: 100
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          height: 100,
+          width: 100
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          height: 100,
+          width: 100
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
+          height: 100,
+          width: 100
+        }}
+      />
+    </View>
   );
 }
-
-const containerStyle = { backgroundColor: "orange" };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
