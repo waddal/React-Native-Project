1bd6f31ed43302d9e208361c42c4f63557872f31
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-03-07 16:33:14 -0800

    refactor: files reorganized

--- App.js
+++ App.js
@@ -1,57 +1,29 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center", // aligns line determines alignment of items in the line
-        alignContent: "center", // determines alignment of all content within container
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          // flexBasis: 100, // width or height on primary axis
-          // flex: 1, // flexGrow has similar effect
-          flexShrink: 1, // if item overflows, shrinks to give room to other elements
-          height: 100,
-          width: 400
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          height: 100,
-          width: 100
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          height: 100,
-          width: 100
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          height: 100,
-          width: 100
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
-          height: 100,
-          width: 100
-        }}
-      />
+    <View style={styles.container}>
+      <View style={[styles.child, { backgroundColor: "dodgerblue" }]}></View>
+      <View style={[styles.child, { backgroundColor: "gold" }]}></View>
+      <View style={[styles.child, { backgroundColor: "tomato" }]}></View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    flexWrap: "wrap",
+  },
+  child: {
+    backgroundColor: "grey",
+    height: 100,
+    width: 100,
+  },
+});
