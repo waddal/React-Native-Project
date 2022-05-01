e6168e68b1cafbe0c765ddf42a7854b888345188
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-05-01 15:38:00 -0700

    impressive visual assets added

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -10,13 +10,13 @@ const messages = [
     id: 1,
     title: "T1",
     description: "D1",
-    image: require("../assets/mosh.jpg"),
+    image: require("../assets/jess.jpg"),
   },
   {
     id: 2,
     title: "T2",
     description: "D2",
-    image: require("../assets/mosh.jpg"),
+    image: require("../assets/alan.jpg"),
   },
 ];
 
