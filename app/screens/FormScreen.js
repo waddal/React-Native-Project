cc2acb004e911c696dda5e40ecfbf2a066a2924c
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-05-02 12:57:11 -0700

    Switch boolean component

--- app/screens/FormScreen.js
+++ app/screens/FormScreen.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import { Switch } from 'react-native'
+
 import AppText from "../components/AppText";
 import AppTextInput from "../components/AppTextInput";
-
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 
 function FormScreen() {
   const [firstName, setFirstName] = useState("");
+  const [isNew, setIsNew] = useState(false);
 
   return (
     <Screen>
@@ -20,6 +22,7 @@ function FormScreen() {
         style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
       /> */}
       <AppTextInput placeholder="Username" icon={"email"} />
+      <Switch value={isNew} onValueChange={newValue => setIsNew(newValue)}/>
     </Screen>
   );
 }
