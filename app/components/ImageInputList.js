425999e612889f1c2e3d81ef7193ea855fa76e1e
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-05-24 16:30:33 -0700

    FormImagePicker created, imageInputLists and Formik unite

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput imageUri={uri} onChangeImage={() => onRemoveImage(uri)} />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageUri={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+          <ImageInput
+            style={styles.image}
+            onChangeImage={(uri) => onAddImage(uri)}
+          />
         </View>
-      ))}
-      <ImageInput style={styles.image} onChangeImage={(uri) => onAddImage(uri)} />
+      </ScrollView>
     </View>
   );
 }
